ba222aa77df6674ddcf131a96583f4bf62c73790
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-04-28 18:51:59 +0300

    refactoring code in regexp_example.go

--- lesson1/regexp_example.go
+++ lesson1/regexp_example.go
@@ -4,20 +4,47 @@ import (
 	"regexp"
 )
 
-func isINN(inn string) bool {
-	regINN_10, _ := regexp.Compile(`(\d{10})`)
-	matchesINN_10 := regINN_10.FindAllString(inn, -1)
+// old name: IsINN
+// new name: FindINN
+func FindINN(stringWithINN string) bool {
+	// arguments for function
+	// old name: inn
+	// new name: stringWithINN
 
-	return len(matchesINN_10) != 0
+	// old name: regINN_10
+	// new name: regexpForINN
+	regexpForINN, _ := regexp.Compile(`(\d{10})`)
+
+	// old name: matchesINN_10
+	// new name: matchesForINN
+	matchesForINN := regexpForINN.FindAllString(stringWithINN, -1)
+
+	return len(matchesForINN) != 0
 }
 
-func isPhone(row string) bool {
-	regPhone, _ := regexp.Compile(`(?:\+|\d)[\d\-\(\) ]{9,}\d`)
-	return regPhone.FindString(row) != ""
+// old name: IsPhone
+// new name: FindPhone
+func FindPhone(stringWithPhone string) bool {
+	// arguments for function
+	// old name: row
+	// new name: stringWithPhone
+
+	// old name: regPhone
+	// new name: regexpForPhone
+	regexpForPhone, _ := regexp.Compile(`(?:\+|\d)[\d\-\(\) ]{9,}\d`)
+	return regexpForPhone.FindString(stringWithPhone) != ""
 }
 
-func isEmail(row string) bool {
-	regMail, _ := regexp.Compile(`\b[\.0-9a-zA-Z]+@[a-zA-Z\.]+\b`)
+// old name: IsEmail
+// new name: FindEmail
+func FindEmail(stringWithEmail string) bool {
+	// arguments for function
+	// old name: row
+	// new name: stringWithEmail
 
-	return regMail.FindString(row) != ""
+	// old name: regMail
+	// new name: regexpForEmail
+	regexpForEmail, _ := regexp.Compile(`\b[\.0-9a-zA-Z]+@[a-zA-Z\.]+\b`)
+
+	return regexpForEmail.FindString(stringWithEmail) != ""
 }
